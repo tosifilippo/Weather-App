7439148fde0d80a2c1604af818e2c99c7618b079
Filippo <tosifilippo@outlook.it>
2021-03-15 04:05:40 +0100

    added simple function that console logs weather in london

---
+++ src/index.js
@@ -0,0 +1,7 @@
+async function getWeather() {
+  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=517bdda0ae62f37b25ca4c3e3a3cc910', { mode: 'cors' });
+  const weatherData = await response.json();
+  // eslint-disable-next-line no-console
+  console.log(weatherData);
+}
+getWeather();
