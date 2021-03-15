7486f75622adad00ea53dffafb6177ca04eb7724
Filippo <tosifilippo@outlook.it>
2021-03-15 07:55:14 +0100

    split data into different paragraphs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const text = document.getElementById('text');
 const submit = document.getElementById('submit');
-const display = document.getElementById('display');
 const form = document.getElementById('form');
+const weatherDescriptionPara = document.getElementById('weather-description');
+const cityPara = document.getElementById('city');
+const temperaturePara = document.getElementById('temperature');
+const feelsLikePara = document.getElementById('feels-like');
+const windPara = document.getElementById('wind');
+const humidityPara = document.getElementById('humidity');
 
 async function getWeather(location) {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=517bdda0ae62f37b25ca4c3e3a3cc910&units=metric`, { mode: 'cors' });
@@ -15,7 +20,12 @@ async function getWeather(location) {
   const feelsLike = weatherData.main.feels_like;
   const wind = weatherData.wind.speed;
   const { humidity } = weatherData.main;
-  display.innerHTML = `${description}<br>${city}, ${country}<br>${temp}°C <br> Feels like: ${feelsLike}°C<br>Wind: ${wind} Km/h<br>Humidity: ${humidity}%`;
+  weatherDescriptionPara.innerHTML = description;
+  cityPara.innerHTML = `${city}, ${country}`;
+  temperaturePara.innerHTML = `${temp}°C`;
+  feelsLikePara.innerHTML = `Feels like: ${feelsLike}°C`;
+  windPara.innerHTML = `Wind: ${wind} Km/h`;
+  humidityPara.innerHTML = `Humidity: ${humidity}%`;
 }
 
 function handleSubmit(e) {
