f02bebff5eba7dbf44346a8db9dcae9c7dc13568
Filippo <tosifilippo@outlook.it>
2021-03-15 11:16:21 +0100

    added radio buttons to switch units to imperial

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const feelsLikePara = document.getElementById('feels-like');
 const windPara = document.getElementById('wind');
 const humidityPara = document.getElementById('humidity');
 const errorMessage = document.getElementById('error');
+const metric = document.getElementById('metric');
+const imperial = document.getElementById('imperial');
 
-async function getWeather(location) {
+async function getWeather(location, units) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=517bdda0ae62f37b25ca4c3e3a3cc910&units=metric`, { mode: 'cors' });
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=517bdda0ae62f37b25ca4c3e3a3cc910&units=${units}`, { mode: 'cors' });
     const weatherData = await response.json();
     errorMessage.hidden = true;
     const description = weatherData.weather[0].description.toUpperCase();
@@ -23,9 +25,21 @@ async function getWeather(location) {
     const { humidity } = weatherData.main;
     weatherDescriptionPara.innerHTML = description;
     cityPara.innerHTML = `${city}, ${country}`;
-    temperaturePara.innerHTML = `${temp}°C`;
-    feelsLikePara.innerHTML = `Feels like: ${feelsLike}°C`;
-    windPara.innerHTML = `Wind: ${wind} Km/h`;
+    if (metric.checked) {
+      temperaturePara.innerHTML = `${temp}°C`;
+    } else {
+      temperaturePara.innerHTML = `${temp}°F`;
+    }
+    if (metric.checked) {
+      feelsLikePara.innerHTML = `Feels like: ${feelsLike}°C`;
+    } else {
+      feelsLikePara.innerHTML = `Feels like: ${feelsLike}°F`;
+    }
+    if (metric.checked) {
+      windPara.innerHTML = `Wind: ${wind} Km/h`;
+    } else {
+      windPara.innerHTML = `Wind: ${wind} Mph`;
+    }
     humidityPara.innerHTML = `Humidity: ${humidity}%`;
   } catch (error) {
     errorMessage.hidden = false;
@@ -34,10 +48,14 @@ async function getWeather(location) {
 
 function handleSubmit(e) {
   e.preventDefault();
-  getWeather(text.value);
+  if (metric.checked) {
+    getWeather(text.value, metric.id);
+  } else {
+    getWeather(text.value, imperial.id);
+  }
 }
 
 submit.addEventListener('click', handleSubmit);
 form.addEventListener('submit', handleSubmit);
 
-getWeather('Pescantina');
+getWeather('Pescantina', 'metric');
