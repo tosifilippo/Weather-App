48984731325545e4abd46d10e6988db6c01896d3
Filippo <tosifilippo@outlook.it>
2021-03-15 06:59:25 +0100

    form can now be submitted on enter and on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const text = document.getElementById('text');
 const submit = document.getElementById('submit');
 const display = document.getElementById('display');
+const form = document.getElementById('form');
 
 async function getWeather(location) {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location},&APPID=517bdda0ae62f37b25ca4c3e3a3cc910&units=metric`, { mode: 'cors' });
@@ -17,8 +18,12 @@ async function getWeather(location) {
   display.innerHTML = `${description}<br>${city}, ${country}<br>${temp}°C <br> Feels like: ${feelsLike}°C<br>Wind: ${wind} Km/h<br>Humidity: ${humidity}%`;
 }
 
-submit.addEventListener('click', () => {
+function handleSubmit(e) {
+  e.preventDefault();
   getWeather(text.value);
-});
+}
+
+submit.addEventListener('click', handleSubmit);
+form.addEventListener('submit', handleSubmit);
 
 getWeather('Pescantina');
